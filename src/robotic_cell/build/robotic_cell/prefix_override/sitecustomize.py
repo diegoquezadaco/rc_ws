5e7bb351d10ffdd60954b2b1385eaae16030bfa4
Diego Quezada <111073579+diegoquezadaco@users.noreply.github.com>
2025-09-18 17:33:23 -0700

    Delete src/robotic_cell/build directory

--- src/robotic_cell/build/robotic_cell/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/acis/robotic_cell_files/rc_ws/src/robotic_cell/install/robotic_cell'
